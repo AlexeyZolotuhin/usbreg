c5060891d81d56d05aeefd631c9e2e5513fa8d53
alexzolt <alesh.psk@gmail.com>
2022-06-07 08:30:06 +0500

    init feature

---
+++ Usbreg/app/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, EqualTo
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    fullname = StringField('Полное имя', validators=[DataRequired()])
+    department = StringField('Подразделение', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Добавить')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Пользователь с таким именем уже существует.')
---
+++ usbparam.py
@@ -0,0 +1,9 @@
+class Statuses:
+    Active   = 'Активная'
+    Broken   = 'Вышла из строя'
+    Returned = 'Сдана'
+
+class Device_type:
+    Flash  = 'USBflash'
+    Reader = 'CardReader'
+    HDD    = 'UsbHDD'
